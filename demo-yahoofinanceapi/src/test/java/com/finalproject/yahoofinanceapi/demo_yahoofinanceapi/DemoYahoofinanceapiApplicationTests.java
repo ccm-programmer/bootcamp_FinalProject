ad112f3bb4a9ae06e4da88c3611092926b6e8946
ivychow <treasureland730@gmail.com>
2025-03-23 01:08:33 +0800

    Re-added demo-yahoofinanceapi as a normal folder

---
+++ demo-yahoofinanceapi/src/test/java/com/finalproject/yahoofinanceapi/demo_yahoofinanceapi/DemoYahoofinanceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.yahoofinanceapi.demo_yahoofinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoYahoofinanceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
